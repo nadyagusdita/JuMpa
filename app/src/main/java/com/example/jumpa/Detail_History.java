5868d6a72c7965c70df869dae3894c343ef1e920
Dhiya Nabila Denta (1911523021) <72001856+DhiyaNabila@users.noreply.github.com>
2021-10-13 23:11:57 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/jumpa/Detail_History.java
@@ -0,0 +1,14 @@
+package com.example.jumpa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detail_History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_history);
+    }
+}
